6b7e4356317596d26b9a17621bba19b0d8c08671
[ibralenko6724809@gmail.com] <ibralenko6724809@gmail.com>
2023-05-22 22:57:38 +0300

    add names

---
+++ index.js
@@ -0,0 +1,4 @@
+const form = document.getElementById("form");
+const username = document.forms.form.username.value;
+const link = document.forms.form.link.value;
+const comment = document.forms.form.comment.value;
