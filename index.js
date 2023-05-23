2aa40d5c22e2f36d4ce95d451b3952e1e2c9b9a6
[ibralenko6724809@gmail.com] <ibralenko6724809@gmail.com>
2023-05-24 00:15:13 +0300

    add function

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const userName = document.forms.form.username.value;
-let rightName = userName.trim();
-let rigtUserName = rightName[0].toUpperCase() + rightName.toLowerCase();
+const LinkInput = document.forms.form.link.value;
+const TextInput = document.forms.form.comment.value;
+const button = document.querySelector(".container__btn");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
+let getName = () => {
+  let rightName = userName.trim();
+  let rigtUserName = rightName[0].toUpperCase() + rightName.toLowerCase();
+};
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+// });
+
+// const text = document.forms.form.comment.value;
+// function checkSpam(str) {
+//   if (text.toLowerCase() === "viagra".toLowerCase())
+//     return (text.textContent = "***");
+//   else if (text.toLowerCase() === "xxx".toLowerCase())
+//     return (text.textContent = "***");
+//   else return text;
+// }
